c11bbc1024151e0bf71e74c4ba5f727d7ac6c846
sscarlethernandez <sscarlethernandez@unicesar.edu.co>
2024-08-26 17:13:35 -0500

    Se conecta el backend con el front

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+      .then(response => response.text()) 
+      .then(data => setMessage(data)) 
+      .catch(error => console.error('Error al conectarse al backend:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Frontend React</h1>
+      <p>{message}</p> 
     </div>
   );
 }
